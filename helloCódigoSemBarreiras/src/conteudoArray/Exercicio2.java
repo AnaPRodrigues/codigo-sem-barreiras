cf2b33be5b59284940b848122063b17737634d27
Ana Rodrigues <aninha-felicio@hotmail.com>
2021-09-07 13:09:33 -0300

    Atualização do dia 07/09 do Curso de Java

---
+++ helloCódigoSemBarreiras/src/conteudoArray/Exercicio2.java
@@ -0,0 +1,32 @@
+package conteudoArray;
+// Escreva um algoritmo que leia números, insira em um array e após mostre a quantidade de números negativos.
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        try(Scanner in = new Scanner(System.in)){
+            var programaContinua = true;
+
+            var numeros = new ArrayList<Integer>();
+
+            while (programaContinua){
+                System.out.println("Digite um numero novo número: ");
+                var variavelteste = in.nextInt();
+                if (variavelteste < 0){
+                    numeros.add(variavelteste);
+                }
+
+                System.out.println("Deseja digitar mais um número: [s] e [n]");
+
+                if(in.next().charAt(0) == 'n'){
+                    programaContinua = false;
+                }
+            }
+            System.out.println("Os números digitados que são negativos foram: ");
+            System.out.println(numeros);
+        }
+    }
+}
